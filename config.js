4c1ce0de2eab3e0bfb864416d85434197a6a671a
usaginomitama <130579466+usaginomitama@users.noreply.github.com>
2025-04-20 02:00:10 +0900

    commited for .gitignore

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+window.CONFIG = {
+    API_KEY: 'AIzaSyDltcb4024smCe3qBWrUl2reb4VnXqp2BA',
+    CLIENT_ID: '238594037265-oelh71fodo8qb4eajv80brerahj6sflj.apps.googleusercontent.com'
+  };
+  
